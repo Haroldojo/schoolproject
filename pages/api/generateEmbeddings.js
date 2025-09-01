2cc2d4485a7c9440ab2ce6d1574e631bf599b001
Haroldojo <156315453+Haroldojo@users.noreply.github.com>
2025-09-01 23:28:59 +0530

    Update generateEmbeddings.js

--- pages/api/generateEmbeddings.js
+++ pages/api/generateEmbeddings.js
@@ -1,5 +1,4 @@
 // pages/api/embeddings.js
-import { OpenAIEmbeddings } from "langchain/embeddings";
 import { connectDB } from "../../lib/db";
 import { cosineSimilarity } from "../../lib/utils"; // we'll write this helper
 
